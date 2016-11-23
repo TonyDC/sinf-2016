0b4862772cd2b74cf5f898f2b2dee3fe323ee312
João Monteiro <joaomiguelmonteiro@gmail.com>
2016-11-23 15:32:07 +0000

    Started integration

--- server/routes/pages/index.js
+++ server/routes/pages/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index');
 });
 
 module.exports = router;
