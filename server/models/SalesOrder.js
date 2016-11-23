55d265d1e1924f7c76410d9828f69d2adf4a4352
João Monteiro <joaomiguelmonteiro@gmail.com>
2016-11-23 16:54:17 +0000

    Integration with sales orders

--- server/models/SalesOrder.js
+++ server/models/SalesOrder.js
@@ -3,8 +3,8 @@ const db = require('../config/db');
 module.exports.getAll = function () {
     return new Promise(function (fulfill, reject) {
         let sales = [];
-        const sale1 = {'id': '1', 'shipping-date': '11/12/2016', 'client-id': '2'};
-        const sale2 = {'id': '2', 'shipping-date': '15/12/2016', 'client-id': '2'};
+        const sale1 = {'id': '1', 'shipping-date': '11/12/2016', 'client': 'Joaquim Almeida', 'client-id': '2'};
+        const sale2 = {'id': '2', 'shipping-date': '15/12/2016', 'client': 'Joaquim Martins', 'client-id': '2'};
         sales.push(sale1);
         sales.push(sale2);
 
@@ -15,10 +15,11 @@ module.exports.getAll = function () {
 module.exports.getItems = function (id) {
     return new Promise(function (fulfill, reject) {
         let items = [];
-        items.push({item: '1', quantity: 2, status: 'ready'});
-        items.push({item: '2', quantity: 4, status: 'not ready'});
+        items.push({item: '1', quantity: 2, name: 'CPU', status: 'ready'});
+        items.push({item: '2', quantity: 4, name: 'Motherboard',  status: 'not ready'});
+        const salesOrder = {'id': '1', 'shipping-date': '11/12/2016', 'client': 'Joaquim Almeida', 'client-id': '2', items: items};
 
-        fulfill(items);
+        fulfill(salesOrder);
     });
 };
 
--- server/routes/pages/index.js
+++ server/routes/pages/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-let router = express.Router();
+const router = express.Router();
+const SalesOrder = require('../../models/SalesOrder');
 
 
 router.get('/', function(req, res, next) {
-    res.render('index');
+    /*
+    const salesOrders = [{date: "2016/11/23", client: "Joaquim Almeida"},
+        {date: "2015/11/23", client: "Alberto Almeida"},
+        {date: "2013/11/23", client: "Joaquim Martins"}];
+        */
+
+    SalesOrder.getAll().then(function(salesOrders) {
+        res.render('index', {salesOrders: salesOrders});
+    });
+});
+
+router.get('/partials/salesOrder/:id', function(req, res, next) {
+   const id = req.params.id;
+
+   SalesOrder.getItems(id).then(function (salesOrder) {
+       res.render('partials/product-modal', {salesOrder:salesOrder, layout: false});
+   })
 });
 
 module.exports = router;
