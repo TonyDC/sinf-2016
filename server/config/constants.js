54ff5d75ddfd12a830a30cb0b2503d588155f014
TonyDC <up201305244@gcloud.fe.up.pt>
2016-12-18 20:00:33 +0000

    Ajustes

--- server/config/constants.js
+++
@@ -1,9 +0,0 @@
-const host = "localhost:52313";
-const path_prefix = "/api/";
-const protocol = "http://";
-
-module.exports.HOST = host;
-module.exports.PATH_PREFIX = path_prefix
-module.exports.PROTOCOL = protocol;
-
-module.exports.URL_HOST = protocol + host + path_prefix;
